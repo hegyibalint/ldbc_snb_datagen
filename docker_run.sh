41743691980c62dc183184553e816f86ae0df6b6
Gabor Szarnyas <szarnyasg@gmail.com>
2019-05-01 13:39:57 +0200

    Pass params.ini instead of env vars

--- docker_run.sh
+++ docker_run.sh
@@ -1,11 +1,4 @@
-#o!/bin/bash
-
-# Parameter serialization
-PARAMS_FILE=params.ini
-echo "ldbc.snb.datagen.generator.scaleFactor:${DATAGEN_SCALE_FACTOR}" >> ${PARAMS_FILE}
-echo "ldbc.snb.datagen.serializer.personSerializer:${DATAGEN_PERSON_SERIALIZER}" >> ${PARAMS_FILE}
-echo "ldbc.snb.datagen.serializer.invariantSerializer:${DATAGEN_INVARIANT_SERIALIZER}" >> ${PARAMS_FILE}
-echo "ldbc.snb.datagen.serializer.personActivitySerializer:${DATAGEN_PERSON_ACTIVITY_SERIALIZER}" >> ${PARAMS_FILE}
+#!/bin/bash
 
 # Running the generator
 /opt/hadoop-2.6.0/bin/hadoop jar /opt/ldbc_snb_datagen/target/ldbc_snb_datagen-0.2.7-jar-with-dependencies.jar /opt/ldbc_snb_datagen/params.ini
